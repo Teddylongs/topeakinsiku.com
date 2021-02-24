acc85cc1178fb5e78e74567e4301177037c1ebff
Teddylongs <eifemade@gmail.com>
2021-02-24 16:02:58 -0500

    updated sections: Intro, Offers & Meet Tope

--- frontend/src/assets/images/index.js
+++ frontend/src/assets/images/index.js
@@ -1,8 +1,11 @@
-import HeroBkg from './header-bkg.gif'
-import LogoBlack from './logo-black.png'
-
-
-export {
-    HeroBkg,
-    LogoBlack
-}
+import HeroBkg from "./header-bkg.gif";
+import LogoBlack from "./logo-black.png";
+import HiThere from "./hi-there.png";
+import MeetTopeImg from "./meet-tope.png";
+import MeetTope from "./meet-tope-title.png"
+import Scribble from "./scribble.png"
+import Offers from "./offers.png"
+import Events from "./events.png"
+import Podcasts from "./podcasts.png"
+import Mentorship from "./mentorship.png"
+export { Events, HeroBkg, LogoBlack, HiThere, MeetTope, MeetTopeImg, Mentorship, Offers, Podcasts, Scribble };
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,41 +1,102 @@
 import React from "react";
 import JoinInnerCircle from "../components/JoinInnerCircle";
-import {HeroBkg} from '../assets/images'
+import {
+  Events,
+  HeroBkg,
+  HiThere,
+  MeetTopeImg,
+  MeetTope,
+  Scribble,
+  Offers,
+  Podcasts,
+  Mentorship,
+} from "../assets/images";
 const HomePage = () => {
   return (
-    <div className='container'>
-      <section id='introduction'>
-        <div className='row'>
-          <div className='col-6 align-items-center justify-content-center'>
-            <h1>Hi There! I'm Tope</h1>
-            <p>
-              Wife, Mother, Pastor, Author, Mentor, <br />
-              Podcaster, Traveller. Welcome to my classroom, <br />
-              podcast and all things JESUS!
-            </p>
-          </div>
-          <div className='col-6'></div>
-        </div>
-      </section>
-      <section id='offers'>
-        <div className='row'>
-          <h1>Here's what I have for you</h1>
-        </div>
-        <div className='row'>
-          <div className='col-4'>Podcasts</div>
-          <div className='col-4'>Mentorship</div>
-          <div className='col-4'>Events</div>
-        </div>
-      </section>
-    <section id='meet-tope'>
-        <div className="row">
-            <div className="col-sm-6">Picture</div>
-            <div className="col-sm-6">
-                <h1>Meet Tope</h1>
+    <div>
+      <section id='introduction' className='my-5'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6 align-items-center justify-content-center my-5'>
+              <img
+                className='mx-auto img-fluid mb-1'
+                src={HiThere}
+                alt="Hi there! I'm Tope"
+              />
+              <p>
+                Wife, Mother, Pastor, Author, Mentor, <br />
+                Podcaster, Traveller. Welcome to my classroom, <br />
+                podcast and all things JESUS!
+              </p>
             </div>
+            <div className='col-6'></div>
+          </div>
         </div>
-    </section>
-    <JoinInnerCircle />
+      </section>
+      <section id='offers' className='py-5 my-5'>
+        <div className='container'>
+          <div className='row mb-5'>
+            <div className='col-sm-4'>
+              <img
+                className='mx-auto img-fluid'
+                src={Offers}
+                alt="Hi there! I'm Tope"
+              />
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='col-sm-4'>
+              <img
+                className='mx-auto img-fluid'
+                src={Podcasts}
+                alt="Hi there! I'm Tope"
+              />
+            </div>
+            <div className='col-sm-4'>
+            <img
+                className='mx-auto img-fluid'
+                src={Mentorship}
+                alt="Hi there! I'm Tope"
+              />
+            </div>
+            <div className='col-sm-4'>
+            <img
+                className='mx-auto img-fluid'
+                src={Events}
+                alt="Hi there! I'm Tope"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id='meet-tope' className='my-5'>
+        <div className='container'>
+          <div className='row py-5'>
+            <div className='col-sm-7'>
+              <div className='text-center'>
+                <img className='img-fluid ' src={MeetTopeImg} alt='image' />
+              </div>
+            </div>
+            <div className='col-sm-5 py-5 px-5'>
+              <div className='row mx-auto'>
+                <img className='img-fluid mb-1' src={MeetTope} alt='image' />
+                <img className='img-fluid mb-5' src={Scribble} alt='image' />
+                <div className='mx-auto justify-content-center'>
+                  <h3 className='mb-4'>I’m a Jesus girl with a passion</h3>
+                  <h3 className='mb-4'>I’m married to my best friend </h3>
+                  <h3 className='mb-4'>
+                    I’m the co-pastor to a <br />
+                    congregation of champions
+                  </h3>
+                  <h3 className='mb-4'>But there’s more… </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <JoinInnerCircle />
     </div>
   );
 };
